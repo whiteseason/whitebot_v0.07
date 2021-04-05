1188899a01c81e982be28c8260ae2b806817acde
whiteseason <whiteandseason@outlook.com>
2021-04-05 10:55:21 +0800

    UserMapper相关

---
+++ src/main/java/live/whiteseason/whitebot/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package live.whiteseason.whitebot.mapper;
+
+import live.whiteseason.whitebot.domain.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author whiteseason
+ * @since 2021-02-10
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
