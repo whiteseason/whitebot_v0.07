161a5cbbec5037a3d7180bf8e297b8a039322992
whiteseason <whiteandseason@outlook.com>
2021-04-05 10:19:21 +0800

    调整Retrofit的Gson处理流程，使其适应V1 API相关操作

---
+++ src/main/java/live/whiteseason/whitebot/config/retrofit/converter/CustomGsonResponseBodyConverter.java
@@ -0,0 +1,47 @@
+package live.whiteseason.whitebot.config.retrofit.converter;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+final class CustomGsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+    private final Gson gson;
+    private final TypeAdapter<T> adapter;
+
+    CustomGsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+        this.gson = gson;
+        this.adapter = adapter;
+    }
+
+    @Override
+    public T convert(ResponseBody value) throws IOException {
+        String response = value.string();
+
+        MediaType contentType = value.contentType();
+        Charset charset = contentType != null ? contentType.charset(UTF_8) : UTF_8;
+        InputStream inputStream = new ByteArrayInputStream(response.substring(1,response.length()-1).getBytes());
+        Reader reader = new InputStreamReader(inputStream, charset);
+        JsonReader jsonReader = gson.newJsonReader(reader);
+
+        try {
+            T result = adapter.read(jsonReader);
+            if (jsonReader.peek() != JsonToken.END_DOCUMENT) {
+                throw new JsonIOException("JSON document was not fully consumed.");
+            }
+            return result;
+        } finally {
+            value.close();
+        }
+    }
+}
