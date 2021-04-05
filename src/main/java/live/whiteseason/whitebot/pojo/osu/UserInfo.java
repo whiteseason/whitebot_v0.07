39bccdf073803038ff90ea54d5b626aa2ac90729
whiteseason <whiteandseason@outlook.com>
2021-04-05 10:41:44 +0800

    使用新的方法过滤字段，使得请求实体类更为简洁

--- src/main/java/live/whiteseason/whitebot/pojo/osu/UserInfo.java
+++ src/main/java/live/whiteseason/whitebot/pojo/osu/UserInfo.java
@@ -1,5 +1,6 @@
 package live.whiteseason.whitebot.pojo.osu;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
@@ -13,25 +14,35 @@ public class UserInfo {
     /**
      * 这个字段不写入数据库
      */
+    @Expose
     @SerializedName("username")
     private String userName;
+    @Expose
     private Integer mode;
+    @Expose
     private int userId;
+    @Expose
     private int count300;
+    @Expose
     private int count100;
+    @Expose
     private int count50;
+    @Expose
     @SerializedName("playcount")
     private int playCount;
+    @Expose
     private float accuracy;
+    @Expose
     private float ppRaw;
-    private long rankedScore;
-    private long totalScore;
-    private float level;
+//    private long rankedScore;
+//    private long totalScore;
+//    private float level;
+    @Expose
     private int ppRank;
-    private int countRankSs;
-    private int countRankSsh;
-    private int countRankS;
-    private int countRankSh;
-    private int countRankA;
-    private LocalDate queryDate;
+//    private int countRankSs;
+//    private int countRankSsh;
+//    private int countRankS;
+//    private int countRankSh;
+//    private int countRankA;
+//    private LocalDate queryDate;
 }
