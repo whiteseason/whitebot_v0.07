5da5fe4194618a4de5fb18a749102cd32f444462
whiteseason <whiteandseason@outlook.com>
2021-04-05 10:54:39 +0800

    retrofit请求器管理类

---
+++ src/main/java/live/whiteseason/whitebot/config/retrofit/RetrofitAdapterConfig.java
@@ -0,0 +1,57 @@
+package live.whiteseason.whitebot.config.retrofit;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import live.whiteseason.whitebot.config.retrofit.converter.CustomGsonConverterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * @author whiteseason
+ */
+@Configuration
+public class RetrofitAdapterConfig {
+    /**
+     * 这里的Gson建立是为了自定义处理json的方法，可以去网上查询相关用法
+     */
+    Gson gson =  new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+    /**
+     * @return v1接口
+     */
+    @Lazy
+    @Bean("OsuAdapter")
+    public Retrofit getOsuAdapter() {
+        /*
+         * baseUrl("https://osu.ppy.sh"):指定基本的URL，
+         * API接口中的URL是相对于该URL的路径的，
+         * 不能少了协议名，例如写成：localhost:8081就不行
+         * CustomGsonConverterFactory.create(gson)中的gson引用是为了可以将自定义的gson处理方法添加到retrofit中，让他能按照我们想要的处理方法处理
+         */
+        return new Retrofit.Builder()
+                .baseUrl("https://osu.ppy.sh")
+                .addConverterFactory(CustomGsonConverterFactory.create(gson))
+                .build();
+    }
+    /**
+     * @return v2接口
+     */
+    @Bean("OsuV2Adapter")
+    public Retrofit getOsuV2Adapter() {
+
+        return new Retrofit.Builder()
+                .baseUrl("https://osu.ppy.sh")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+    /**
+     * v2Token请求
+     */
+
+}
